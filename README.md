7dd69b0503e76020d56057cf87881e504f2705d1
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-08-28 15:17:43 -0700

    Further Investigation of Vine Trustworthiness

--- README.md
+++ README.md
@@ -18,18 +18,41 @@ Google Colab, PySpark, Amazon RDS, postgreSQL
 
 ### Trustworthiness Analysis of Amazon Reviews
 
-* Minimum, average and maxiumum Star Rating
+The definition of a Vine customer is: "Amazon Vine invites the most trusted reviewers on Amazon to post opinions about new and pre-release items to help their fellow customers make informed purchase decisions. Amazon invites customers to become Vine Voices based on their reviewer rank, which is a reflection of the quality and helpfulness of their reviews as judged by other Amazon customers. Amazon provides Vine members with free products that have been submitted to the program by participating vendors. Vine reviews are the independent opinions of the Vine Voices. The vendor cannot influence, modify or edit the reviews. Amazon does not modify or edit Vine reviews, as long as they comply with our posting guidelines. A Vine review is identified with the green stripe Customer review from the Amazon Vine Program." [<a href="https://www.amazon.com/gp/vine/help?ie=UTF8" target="_blank">What is Amazon Vine</a>]
+
+The following investigation drills down through the data to determine if Vine reviews are trustworthy.
+
+* Minimum, average and maxiumum Star Rating. There are not significant differences between vine/not vine customers.
   
-    ![Vine Star Rating](images/query_1a.png)
+    ![Vine Star Rating, query_1a](images/query_1a.png)
 
-* Minimum, average and maxiumum Helpful Votes count
+* Minimum, average and maxiumum Helpful Votes count. There is a very large difference in vine/not vine customers for max_helpful_votes that is worth investigating further.
     
-    ![Vine Helpful Votes](images/query_1b.png)
+    ![Vine Helpful Votes, query_1b](images/query_1b.png)
 
-* Minimum, average and maxiumum Total Votes count
+* Minimum, average and maxiumum Total Votes count.  There is a very large difference in vine/not vine customers for max_total_votes that is worth investigating further.
 
-    ![Vine Total Votes](images/query_1c.png)
+    ![Vine Total Votes, query_1c](images/query_1c.png)
 
+* Statistics for customers with the most max_helpful_votes.  Vine customers do not show up in the top 10 max_helpful_votes reviews. 
+  
+    ![Maximum max_helpful_votes, query_2a](images/query_2a.png)
+
+* Statistics for Vine customers with the most max_helpful_votes.  The Vine customers most max_helpful_votes reviews don't have very many reviews. I would have expected vine customers to have 10-20 reviews each.  Meaning they have reviewed multiple items, but not so many items that they look like a bot.
+  
+    ![Vine only Maximum max_helpful_votes, query_3a](images/query_3a.png)
+
+* Statistics for customers with the most reviews.  Vine customers do not show up in the top 10.  The reviewers with the most reviews have counts that are unreasonably high. I don't see how a customer could read thousands of ebooks in a year.
+  
+    ![Maximum review count, query_4a](images/query_4a.png)
+
+* Statistics for Vine customers with the most reviews.  Amazon Vine customers produce very few reviews in the ebooks category with the exception of 1 customer.  
+
+    ![Vine Maximum review count, query_4b](images/query_4b.png)
+
+* Statistics for the Vine customers with the most reviews to see if they also have non-Vine reviews.  All of these customers have a reasonable number of non-Vine ebook reviews that have at least a handful of review votes.  These statistics appear to show that Vine reviews are trustworthy. 
+
+    ![Vine vs non-vine reviews, query_5a](images/query_5a.png)
 
 
 ### Replication Instructions
--- level-2/query.sql
+++ level-2/query.sql
@@ -50,10 +50,10 @@ select v.vine,
  group by v.vine;
  
 -----------------------
--- Investigate large vote counts
+-- Investigate large vote counts for all customers
 -----------------------
 
- -- 2a Review stats by max_helpful_votes
+-- 2a Review stats by most max_helpful_votes
  select r.customer_id,
 	   v.vine,
 	   count(r.customer_id) as review_count,
@@ -71,3 +71,142 @@ group by r.customer_id, v.vine
 order by max_helpful_votes desc
  limit 10;
  
+-- 2b Review stats by min max_helpful_votes.  All stats are 0.
+ select r.customer_id,
+	   v.vine,
+	   count(r.customer_id) as review_count,
+	   min(v.helpful_votes) as min_helpful_votes,
+	   avg(v.helpful_votes) as avg_helpful_votes,
+	   max(v.helpful_votes) as max_helpful_votes,
+	   min(v.total_votes) as min_total_votes,
+	   avg(v.total_votes) as avg_total_votes,
+	   max(v.total_votes) as max_total_votes
+  from vine_table as v
+  	   inner join review_id_table as r
+	   on v.review_id = r.review_id
+where v.vine is not NULL
+group by r.customer_id, v.vine
+order by max_helpful_votes asc
+ limit 10;
+
+-- 2c Review stats by most max_total_votes. Very minor differences from 2a. 
+ select r.customer_id,
+	   v.vine,
+	   count(r.customer_id) as review_count,
+	   min(v.helpful_votes) as min_helpful_votes,
+	   avg(v.helpful_votes) as avg_helpful_votes,
+	   max(v.helpful_votes) as max_helpful_votes,
+	   min(v.total_votes) as min_total_votes,
+	   avg(v.total_votes) as avg_total_votes,
+	   max(v.total_votes) as max_total_votes
+  from vine_table as v
+  	   inner join review_id_table as r
+	   on v.review_id = r.review_id
+where v.vine is not NULL
+group by r.customer_id, v.vine
+order by max_total_votes desc
+ limit 10;
+ 
+-- 2d Review stats by min max_total_votes.  All stats are 0.
+ select r.customer_id,
+	   v.vine,
+	   count(r.customer_id) as review_count,
+	   min(v.helpful_votes) as min_helpful_votes,
+	   avg(v.helpful_votes) as avg_helpful_votes,
+	   max(v.helpful_votes) as max_helpful_votes,
+	   min(v.total_votes) as min_total_votes,
+	   avg(v.total_votes) as avg_total_votes,
+	   max(v.total_votes) as max_total_votes
+  from vine_table as v
+  	   inner join review_id_table as r
+	   on v.review_id = r.review_id
+where v.vine is not NULL
+group by r.customer_id, v.vine
+order by max_total_votes asc
+ limit 10;
+ 
+-----------------------
+-- Investigate large vote counts for vine only customers
+-----------------------
+
+-- 3a Review stats by most max_helpful_votes
+ select r.customer_id,
+	   count(r.customer_id) as review_count,
+	   min(v.helpful_votes) as min_helpful_votes,
+	   avg(v.helpful_votes) as avg_helpful_votes,
+	   max(v.helpful_votes) as max_helpful_votes,
+	   min(v.total_votes) as min_total_votes,
+	   avg(v.total_votes) as avg_total_votes,
+	   max(v.total_votes) as max_total_votes
+  from vine_table as v
+  	   inner join review_id_table as r
+	   on v.review_id = r.review_id
+where v.vine = 'Y'
+group by r.customer_id
+order by max_helpful_votes desc
+ limit 10;
+ 
+-----------------------
+-- Investigate review counts 
+-----------------------
+
+-- 4a Review stats by customers with the most reviews
+ select r.customer_id,
+	   v.vine,
+	   count(r.customer_id) as review_count,
+	   min(v.helpful_votes) as min_helpful_votes,
+	   avg(v.helpful_votes) as avg_helpful_votes,
+	   max(v.helpful_votes) as max_helpful_votes,
+	   min(v.total_votes) as min_total_votes,
+	   avg(v.total_votes) as avg_total_votes,
+	   max(v.total_votes) as max_total_votes
+  from vine_table as v
+  	   inner join review_id_table as r
+	   on v.review_id = r.review_id
+where v.vine is not NULL
+group by r.customer_id, v.vine
+order by review_count desc
+ limit 10;
+ 
+-- 4b Review counts for Vine customers
+ select r.customer_id,
+	   count(r.customer_id) as review_count,
+	   min(v.helpful_votes) as min_helpful_votes,
+	   avg(v.helpful_votes) as avg_helpful_votes,
+	   max(v.helpful_votes) as max_helpful_votes,
+	   min(v.total_votes) as min_total_votes,
+	   avg(v.total_votes) as avg_total_votes,
+	   max(v.total_votes) as max_total_votes
+  from vine_table as v
+  	   inner join review_id_table as r
+	   on v.review_id = r.review_id
+where v.vine = 'Y'
+group by r.customer_id
+order by review_count desc
+ limit 10;
+ 
+-----------------------
+-- Investigate if the top voted vine customers have vine and not-vine reviews
+-----------------------
+
+-- 5a Review stats by customer_id
+ select r.customer_id,
+	   v.vine,
+	   count(r.customer_id) as review_count,
+	   min(v.helpful_votes) as min_helpful_votes,
+	   avg(v.helpful_votes) as avg_helpful_votes,
+	   max(v.helpful_votes) as max_helpful_votes,
+	   min(v.total_votes) as min_total_votes,
+	   avg(v.total_votes) as avg_total_votes,
+	   max(v.total_votes) as max_total_votes
+  from vine_table as v
+  	   inner join review_id_table as r
+	   on v.review_id = r.review_id
+where r.customer_id = 33376665
+   or r.customer_id = 16829939
+   or r.customer_id = 42800127
+   or r.customer_id = 52782976
+   or r.customer_id = 51940534
+group by r.customer_id, v.vine
+order by r.customer_id desc
+ limit 10;
